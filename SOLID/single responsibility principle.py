d4d932ae9125c497773d0f79c9b8caadae7bedb9
Aasam Sri Ram <101095695+SreeramKanekiKen@users.noreply.github.com>
2025-08-22 19:05:19 +0530

    Single Responsibilty Principle

---
+++ SOLID/single responsibility principle.py
@@ -0,0 +1,13 @@
+class UserAuthentication:
+    def authenticate_user(self, username, password):
+        print(f"Authenticating user {username} with password {password}")
+        
+class UserProfile:
+    def user_profile(self, username):
+        print(f"Displaying profile for user {username}")
+        
+class EmailNotification:
+    def send_email_notification(self, username, message):
+        print(f"Sending email to {username}: {message}")
+        
+        
